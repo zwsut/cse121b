af2b856d654bc04f7d42d2045b4c74823d8314a6
zwsut <zwsutherland@gmail.com>
2024-02-26 15:39:40 -0500

    w02 task complete, files added to sandbox

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,35 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Zach Sutherland";
+let currentYear = "2024";
+const profilePicture = "images/cse.jpg"
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+let favoriteFoods = ["Dumplings", "Chocolate", "Potatoes", "Birria"];
+foodElement.textContent = favoriteFoods.join(", ");
+let addedFood = "Mandarin Chicken";
+favoriteFoods.push(addedFood);
+foodElement.innerHTML += `<br>${favoriteFoods.join(", ")}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods.join(", ")}`;
+
+
+
+
+
+
