dcb432d4efbd2ffa01d2d1eda8133d0f9a835e77
zwsut <zwsutherland@gmail.com>
2024-02-26 07:51:38 -0500

    w01task complete

---
+++ sandbox/debugging.js
@@ -0,0 +1,20 @@
+// const pi = 3.14;
+// var radius = 3;
+// circleArea(radius);
+
+// radius = 4;
+// circleArea(radius);
+
+// function circleArea(radius) {
+//     let area = radius * radius * pi;
+//     console.log("Area: ", area);
+// }
+
+const PI = 3.14;
+var area = 0;
+function circleArea(radius) {
+  area = radius * radius * PI;
+}
+circleArea(3);
+console.log(area);
+
